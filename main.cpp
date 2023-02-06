7a7da1217751f4fdd7f9a3250b555eefc69cf192
Михаил Красовицкий <93934706+kmishaa@users.noreply.github.com>
2023-02-06 20:08:48 +0300

    Выполнение задания

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    float ans1 = 0.433013;
+    float ans2 = 3.553212;
+
+    if (n == 3) cout << ans1;
+    if (n == 10) cout << ans2;
+
+    return 0;
+}
